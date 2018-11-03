f9439fc0516a4452160f1c79121bb8acc4e1cabf
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-11-04 08:13:37 +1300

    server running
    server running

---
+++ src/server/app.js
@@ -0,0 +1,11 @@
+let express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+app.listen(3000, () => {
+  console.log("MERN Boilerplate listening on port 3000!");
+});
