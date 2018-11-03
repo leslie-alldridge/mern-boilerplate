3420f26e08db105ec96ccb53387361eaca5b5b22
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-11-04 10:48:39 +1300

    added mongo connection
    added mongo connection

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import utils from "./helpers/utils.js";
 import path from "path";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 global.path = path;
 global.dotenv = dotenv;
@@ -10,6 +12,15 @@ utils.loadENV();
 const app = express();
 
 app.use(express.static(path.resolve(process.cwd(), "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose
+  .connect(
+    "mongodb://heroku_f66trml5:m1n99vr4t31j2ipncmu79cboui@ds151393.mlab.com:51393/heroku_f66trml5",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.get("/api", (req, res) => {
   res.send("Express to the rescue!");
